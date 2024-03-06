f43dcdadd1a1b3cddd364e4fdf980d74542167c5
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-03-06 15:26:44 +0530

    update the colorscheme
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,15 @@
 -- Configure LazyVim to load gruvbox
+vim.opt.background = "light" 
+vim.api.nvim_set_hl(0, "Normal", { bold = true })
 return {
   { "olimorris/onedarkpro.nvim", priority = 1000 },
   { "ellisonleao/gruvbox.nvim" },
   { "zootedb0t/citruszest.nvim" },
+  { "neanias/everforest-nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "citruszest",
+      colorscheme = "everforest",
     },
   },
 }
